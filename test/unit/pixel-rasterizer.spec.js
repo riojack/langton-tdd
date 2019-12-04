a97a0ecab38524c49963f175831a1aceaf5cc723
riojack <ayjay122@gmail.com>
2019-12-04 15:42:31 -0600

    refactor: DRY up tests

--- test/unit/pixel-rasterizer.spec.js
+++ test/unit/pixel-rasterizer.spec.js
@@ -1,53 +1,41 @@
 import PixelRasterizer from '../../src/pixel-rasterizer';
 
+const COLOR = { r: 5, g: 6, b: 7, a: 255 };
+
 describe('Pixel Rasterizer', () => {
-  test('puts image data at the given x,y coordinates', () => {
-    const fakeContext = {
+  let fakeContext, rasterizer;
+
+  beforeEach(() => {
+    fakeContext = {
       createImageData: jest.fn().mockReturnValueOnce({ data: [], height: 0, width: 0 }),
       putImageData: jest.fn()
     };
 
-    const rasterizer = new PixelRasterizer();
-    const color = {
-      r: 5,
-      g: 6,
-      b: 7,
-      a: 255
-    };
+    rasterizer = new PixelRasterizer();
+  });
+
+  test('puts image data at the given x,y coordinates', () => {
     const x = 15;
     const y = 19;
 
-    rasterizer.rasterizeOne(fakeContext, color, x, y);
+    rasterizer.rasterizeOne(fakeContext, COLOR, x, y);
 
     expect(fakeContext.putImageData).toHaveBeenCalledTimes(1);
     expect(fakeContext.putImageData).toHaveBeenCalledWith(expect.anything(), x, y);
   });
 
   test('puts 1-by-1 pixel image data with the given color data', () => {
-    const fakeContext = {
-      createImageData: jest.fn().mockReturnValueOnce({ data: [], height: 0, width: 0 }),
-      putImageData: jest.fn()
-    };
-
-    const rasterizer = new PixelRasterizer();
-    const color = {
-      r: 5,
-      g: 6,
-      b: 7,
-      a: 255
-    };
-
-    rasterizer.rasterizeOne(fakeContext, color, 0, 0);
+    rasterizer.rasterizeOne(fakeContext, COLOR, 0, 0);
 
     const [[imageData]] = fakeContext.putImageData.mock.calls;
     const { data } = imageData;
 
     expect(fakeContext.createImageData).toHaveBeenCalledWith(1, 1);
     expect(data).toEqual([
-      color.r,
-      color.g,
-      color.b,
-      color.a
+      COLOR.r,
+      COLOR.g,
+      COLOR.b,
+      COLOR.a
     ]);
   });
 });
